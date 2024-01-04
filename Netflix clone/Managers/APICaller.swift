ac06cd249435bd4708bc622be6e1713f4b6e9636
Leonardo Macedo <leonardomacedo@MacBook-Air-de-Leonardo.local>
2024-01-04 08:09:26 -0300

    sending url requests and parsing json response

---
+++ Netflix clone/Managers/APICaller.swift
@@ -0,0 +1,37 @@
+//
+//  APICaller.swift
+//  Netflix clone
+//
+//  Created by Leonardo Macedo on 04/01/24.
+//
+
+import Foundation
+
+struct Constants {
+    static let API_KEY = "1fa1ffc389af73a35593123b2805ba1b"
+    static let baseURL = "https://api.themoviedb.org"
+}
+
+class APICaller {
+    static let shared = APICaller()
+    
+    
+    func getTrendingMovies(completion: @escaping (String) -> Void) {
+        
+        guard let url = URL(string: "\(Constants.baseURL)/3/trending/all/day?api_key=\(Constants.API_KEY)") else {return}
+        
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _, error in
+            guard let data = data, error == nil else {
+                return
+            }
+            
+            do {
+                let results = try JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed)
+                print(results)
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+        task.resume()
+    }
+}
