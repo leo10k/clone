e5804d524d437cea9198b635d80f4e5df8645d31
Leonardo Macedo <leonardomacedo@MacBook-Air-de-Leonardo.local>
2024-01-04 10:18:40 -0300

    consuming api to fetch data for each section

--- Netflix clone/Controllers/HomeViewController.swift
+++ Netflix clone/Controllers/HomeViewController.swift
@@ -30,7 +30,7 @@ class HomeViewController: UIViewController {
         let headerView = HeroHeaderUIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
         homeFeedTable.tableHeaderView = headerView
         
-        getTrendingMovies()
+        fetchData()
     }
     
     
@@ -51,16 +51,37 @@ class HomeViewController: UIViewController {
     }
     
     
-    private func getTrendingMovies() {
-        APICaller.shared.getTrendingMovies(completion: { results in
-            switch results {
-            case .success(let movies):
-                print(movies)
-            case .failure(let error):
-                print(error)
-            }
-            
-        })
+    private func fetchData() {
+//        APICaller.shared.getTrendingMovies { results in
+//            switch results {
+//            case .success(let movies):
+//                print(movies)
+//            case .failure(let error):
+//                print(error)
+//            }
+//        }
+        
+//        APICaller.shared.getTrendingTvs { results in
+//            switch results {
+//                case .success(let movies):
+//                    print(movies)
+//                case .failure(let error):
+//                    print(error)
+//                }
+//        }
+        
+//        APICaller.shared.getUpcomingMovies { results in
+//            
+//        }
+        
+//        APICaller.shared.getPopularMovies { results in
+//            
+//        }
+        
+        APICaller.shared.getTopRated { results in
+        
+        }
+        
     }
 
 }
