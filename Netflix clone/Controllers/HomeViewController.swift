2bce0be3dd12e2b5f5c7c4553d3c72b70f5cf411
Leonardo Macedo <leonardomacedo@MacBook-Air-de-Leonardo.local>
2024-01-04 08:35:03 -0300

    creating the model struct

--- Netflix clone/Controllers/HomeViewController.swift
+++ Netflix clone/Controllers/HomeViewController.swift
@@ -52,9 +52,15 @@ class HomeViewController: UIViewController {
     
     
     private func getTrendingMovies() {
-        APICaller.shared.getTrendingMovies { _ in
+        APICaller.shared.getTrendingMovies(completion: { results in
+            switch results {
+            case .success(let movies):
+                print(movies)
+            case .failure(let error):
+                print(error)
+            }
             
-        }
+        })
     }
 
 }
--- Netflix clone/Managers/APICaller.swift
+++ Netflix clone/Managers/APICaller.swift
@@ -12,11 +12,15 @@ struct Constants {
     static let baseURL = "https://api.themoviedb.org"
 }
 
+enum APIError: Error {
+    case failedTogetData
+}
+
 class APICaller {
     static let shared = APICaller()
     
     
-    func getTrendingMovies(completion: @escaping (String) -> Void) {
+    func getTrendingMovies(completion: @escaping (Result<[Movie], Error>) -> Void) {
         
         guard let url = URL(string: "\(Constants.baseURL)/3/trending/all/day?api_key=\(Constants.API_KEY)") else {return}
         
@@ -26,10 +30,10 @@ class APICaller {
             }
             
             do {
-                let results = try JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed)
-                print(results)
+                let results = try JSONDecoder().decode(TredingMoviesResponse.self, from: data)
+                completion(.success(results.results))
             } catch {
-                print(error.localizedDescription)
+                completion(.failure(error))
             }
         }
         task.resume()
