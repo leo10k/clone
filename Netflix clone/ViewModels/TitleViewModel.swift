7d3024b9a7d9b6f05ece859a38f77068291d565b
Leonardo Macedo <leonardomacedo@MacBook-Air-de-Leonardo.local>
2024-01-05 10:29:13 -0300

    creating TitleViewModel

---
+++ Netflix clone/ViewModels/TitleViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TitleViewModel.swift
+//  Netflix clone
+//
+//  Created by Leonardo Macedo on 05/01/24.
+//
+
+import Foundation
+
+struct TitleViewModel {
+    let titleName: String
+    let posterURL: String
+}
