63d428746f3366a20b5b5fb9b4e7c6109026f76a
Leonardo Macedo <leonardomacedo@MacBook-Air-de-Leonardo.local>
2024-01-09 10:36:37 -0300

    refactoring TableViewHeader Hero title

--- Netflix clone/Controllers/Core/HomeViewController.swift
+++ Netflix clone/Controllers/Core/HomeViewController.swift
@@ -17,7 +17,10 @@ enum Sections: Int {
 
 class HomeViewController: UIViewController {
     
-    let sectionTitles : [String] = ["Trending Movies", "Trending Tv", "Popular", "Upcoming Movies", "Top rated"]
+    private var randomTrendingMovie: Title?
+    private var headerView: HeroHeaderUIView?
+    
+    let sectionTitles: [String] = ["Trending Movies", "Trending Tv", "Popular", "Upcoming Movies", "Top rated"]
     
     private let homeFeedTable: UITableView = {
         let table = UITableView(frame: .zero, style: .grouped)
@@ -35,11 +38,26 @@ class HomeViewController: UIViewController {
         
         configureNavbar()
         
-        let headerView = HeroHeaderUIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
+        headerView = HeroHeaderUIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
         homeFeedTable.tableHeaderView = headerView
         
+        configureHeroHeaderView()
     }
     
+    private func configureHeroHeaderView() {
+        
+        APICaller.shared.getTrendingMovies { [weak self] result in
+            switch result {
+            case .success(let titles):
+                let selectedTitle = titles.randomElement()
+                self?.randomTrendingMovie = selectedTitle
+                self?.headerView?.configure(with: TitleViewModel(titleName: selectedTitle?.original_title ?? "", posterURL: selectedTitle?.poster_path ?? ""))
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+        
+    }
     
     private func configureNavbar() {
         var image = UIImage(named: "netflixLogo")
